b925ec08af6e69c63bfc9590bac5a2162189058e
Bryce Mecum <petridish@gmail.com>
2024-04-28 21:40:38 -0800

    Update main code to build a simple Array

--- main.cc
+++ main.cc
@@ -1,8 +1,16 @@
 #include "emscripten.h"
 
+#include <arrow/api.h>
 #include <arrow/status.h>
 
 arrow::Status RunMain() {
+  arrow::Int32Builder builder;
+
+  ARROW_RETURN_NOT_OK(builder.Append(1));
+  ARROW_RETURN_NOT_OK(builder.Append(2));
+  ARROW_RETURN_NOT_OK(builder.Append(3));
+  ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Array> arr, builder.Finish())
+
   return arrow::Status::OK();
 }
 
