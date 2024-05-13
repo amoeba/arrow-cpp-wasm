dddcbb2c33ac80f16f8a8b0c7f531daa0676bfe9
Bryce Mecum <petridish@gmail.com>
2024-05-13 13:59:01 -0800

    Exit out of build.sh if EMSDK isn't defined

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 # TODO: How can I use the pkg-config files in the emsdk sysroot to make the
 # -I/-L arguments automatically populate?
 
+if [ -z "$EMSDK" ]; then
+  echo "$EMSDK not set. Is the emsdk initialized? Exiting."
+  exit 1
+fi
+
 emcc -O3 -s WASM=1 \
   -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
   -s EXPORTED_FUNCTIONS=_main \
